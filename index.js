b3776f145c0a63c328d3533ad617731c16a0c87d
ryntab <57367794+ryntab@users.noreply.github.com>
2024-07-30 12:51:07 -0400

    refactor: :recycle:

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+app.listen(8080, () => {
+    console.log('Server running on port 8080');
+});
+
 const ensureFullyQualifiedDomain = (site) => {
     let url;
 
