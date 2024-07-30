29ae6d923a446522f01b3f8f6759412828225183
ryntab <57367794+ryntab@users.noreply.github.com>
2024-07-30 13:13:39 -0400

    refactor: :recycle: woops add cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import { scan, scanWithQueue } from '@ryntab/wappalyzer-node';
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.listen(8080, () => {
     console.log('Server running on port 8080');
